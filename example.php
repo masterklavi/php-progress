683ef88aed8a3337860ff329885dd519b3961025
Masterklavi <masterklavi@gmail.com>
2017-05-11 20:34:33 +0500

    comments in example

--- example.php
+++ example.php
@@ -4,10 +4,14 @@ use masterklavi\phpprogress\Progress;
 
 require 'lib/Progress.php';
 
+// init progress of 500 tasks
 $progress = new Progress(500);
 
 for ($i = 0; $i < 500; $i++)
 {
-    $progress->show();
+    // some task
     usleep(rand(0, 50000));
+
+    // mark that a task was completed
+    $progress->show();
 }
